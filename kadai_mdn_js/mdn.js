cfdb7ad17c7e7d67834e86cf131b18fcdde92986
Nonchas <168254736+Nonchas@users.noreply.github.com>
2024-08-29 23:14:24 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,8 @@
 const currientDate = new Date();
 
 const year = currientDate.getFullYear();
-const month = currientDate.getMonth();
+const month = currientDate.getMonth() + 1;
 const date = currientDate.getDate();
-
-const zeroPad = (num) => String(num).padStart(2,'0');
 
 const formattedDate = `${year}年${month}月${date}日`;
 console.log(formattedDate);
